3c687f6b270a2ff07b9cc2108309a96562785a33
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-19 12:51:10 +0900

    config 설정

--- fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
+++ fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.example.fourth.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private CorsConfig corsConfig;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       return http
+               .csrf().disable()
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+               .and()
+               .addFilter(corsConfig.corsFilter())
+               .formLogin().disable()
+               .httpBasic().disable()
+               .authorizeHttpRequests(authroize -> authroize.antMatchers("/api/v1/user/**").hasAnyRole("ADMIN, MANAGER", "USER")
+                       .antMatchers("/api/v1/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                       .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                       .anyRequest().permitAll())
+               .build();
+    }
+
 }
