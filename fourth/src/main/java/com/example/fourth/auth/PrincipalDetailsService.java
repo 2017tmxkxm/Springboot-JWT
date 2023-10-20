6bba07f461d48c391accc1858a53ce58a7e59dd0
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-20 12:12:07 +0900

    UserDetails, UserDetailsService 추가

---
+++ fourth/src/main/java/com/example/fourth/auth/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.fourth.auth;
+
+import com.example.fourth.model.User;
+import com.example.fourth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+
+        return new PrincipalDetails(user);
+    }
+
+}
