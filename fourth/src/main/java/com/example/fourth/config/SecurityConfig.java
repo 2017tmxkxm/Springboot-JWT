f3317e951cefb2435cb8e5741abbaa69dba75e24
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-20 12:10:00 +0900

    filter 적용 test

--- fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
+++ fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.fourth.config;
 
+import com.example.fourth.filter.MyFilter1;
+import com.example.fourth.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 import javax.servlet.Filter;
 
@@ -21,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        return http
+               .addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class)
                .csrf().disable()
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                .and()
