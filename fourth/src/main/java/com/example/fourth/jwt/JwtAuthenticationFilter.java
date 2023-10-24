971df90bd204b5f4923de889514aecd9562179a5
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-24 11:30:43 +0900

    Jwt 토큰 생성

---
+++ fourth/src/main/java/com/example/fourth/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.example.fourth.jwt;
+
+import com.example.fourth.auth.PrincipalDetails;
+import com.example.fourth.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Key;
+import java.util.Date;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            ObjectMapper om = new ObjectMapper();
+            User user = om.readValue(request.getInputStream(), User.class);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            return authentication;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        byte[] keyBytes = Decoders.BASE64.decode(JwtProperties.SECRET);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+        String jwtToken =Jwts.builder()
+                .setSubject("cos토큰")
+                .claim("id", principalDetails.getUser().getId())
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(new Date(System.currentTimeMillis() + (60000 * 10))) // 10분
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + jwtToken);
+    }
+}
+
