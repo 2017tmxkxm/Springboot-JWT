ea94fa46e93357959d8a318f2ec46c5ecae1d7e5
2017tmxkxm <2017tmxkxm@gmail.com>
2023-10-24 11:31:02 +0900

    filter 추가

--- fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
+++ fourth/src/main/java/com/example/fourth/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.example.fourth.config;
 
 import com.example.fourth.filter.MyFilter1;
 import com.example.fourth.filter.MyFilter3;
+import com.example.fourth.jwt.JwtAuthenticationFilter;
+import com.example.fourth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
@@ -19,23 +23,38 @@ import javax.servlet.Filter;
 public class SecurityConfig {
 
     @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private CorsConfig corsConfig;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
        return http
-               .addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class)
+               //.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class)
                .csrf().disable()
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                .and()
-               .addFilter(corsConfig.corsFilter())
+               //.addFilter(corsConfig.corsFilter())
                .formLogin().disable()
                .httpBasic().disable()
+               .apply(new MyCustomDsl())
+               .and()
                .authorizeHttpRequests(authroize -> authroize.antMatchers("/api/v1/user/**").hasAnyRole("ADMIN, MANAGER", "USER")
                        .antMatchers("/api/v1/manager/**").hasAnyRole("ADMIN", "MANAGER")
                        .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
                        .anyRequest().permitAll())
                .build();
+    }
+
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(corsConfig.corsFilter())
+                    .addFilter(new JwtAuthenticationFilter(authenticationManager));
+                    //.addFilter(new JwtAuthorizationFilter(authenticationManager, userRepository));
+        }
     }
 
 }
